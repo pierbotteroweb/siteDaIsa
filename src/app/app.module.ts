546bb9acfffa18bc7610d0de9155d12f8d2929eb
unknown <pierbotteroweb@gmail.comm>
2024-05-01 17:59:09 -0300

    components-removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InputComponent } from './input/input.component';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    InputComponent,
-    TestComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
